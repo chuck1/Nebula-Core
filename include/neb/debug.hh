70efe0973e8eaf53c0508978e8582afffa68a3e7
Charles Rymal <charlesrymal@gmail.com>
2014-07-01 19:14:15 -0700

    initial

---
+++ include/neb/debug.hh
@@ -0,0 +1,11 @@
+#ifndef NEBULA_DENUG_HPP
+#define NEBULA_DENUG_HPP
+
+#ifdef _DEBUG
+#	define DEBUG_NEB 1
+#else
+#	define DEBUG_NEB 0
+#endif
+
+#endif
+
---
+++ src/neb/core/app/__core.cpp
@@ -0,0 +1,14 @@
+
+
+#include <neb/core/app/__core.hpp>
+
+void				neb::app::__core::init() {
+}
+neb::core::pose			neb::app::__core::getPose() {
+	return neb::core::pose();
+}
+neb::core::pose			neb::app::__core::getPoseGlobal() {
+	return neb::core::pose();
+}
+
+
