dfacbceced140c8f6a784cdb6b2f39154ba668e9
chuck <charlesrymal@gmail.com>
2015-02-24 14:53:32 -0800

    fix multi defs in final

--- src/neb/core/camera/proj/Perspective.cpp
+++ src/neb/core/camera/proj/Perspective.cpp
@@ -1,5 +1,7 @@
 #include <neb/fnd/camera/proj/Perspective.hpp>
 
+#include <neb/fnd/plug/gfx/camera/proj/Base.hpp>
+
 typedef neb::fnd::camera::proj::Perspective THIS;
 
 THIS::Perspective()
@@ -8,6 +10,9 @@ THIS::Perspective()
 void		THIS::init(parent_t * const & p)
 {
 	setParent(p);
+	
+	if(_M_graphics_object)
+		_M_graphics_object->calculate();
 }
 void		THIS::step(gal::etc::timestep const & p)
 {
