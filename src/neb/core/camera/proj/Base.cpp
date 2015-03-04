179439e78dd814e6de51c260797e8b2a4acddfa1
chuck <charlesrymal@gmail.com>
2015-03-04 11:06:32 -0800

    remove final completely

--- src/neb/core/camera/proj/Base.cpp
+++ src/neb/core/camera/proj/Base.cpp
@@ -20,3 +20,4 @@ void			THIS::calculate_geometry()
 }
 
 
+
--- src/neb/core/context/FBOM.cpp
+++ src/neb/core/context/FBOM.cpp
@@ -1,9 +1,21 @@
 #include <neb/fnd/context/FBOM.hpp>
+#include <neb/fnd/plug/gfx/context/Base.hpp>
 
 typedef neb::fnd::environ::shadow::Point ESP;
 typedef neb::fnd::environ::shadow::Directional ESD;
 
 typedef neb::fnd::context::FBOM THIS;
+
+void				THIS::init(parent_t * const parent)
+{
+	setParent(parent);
+
+	//neb::gfx::context::fbo_multi::init(parent);
+	//if(G::M_graphics_object) 
+	//	_M_graphics_object->init(this);
+}
+
+
 
 std::weak_ptr<ESP>		THIS::createEnvironShadowPoint()
 {
@@ -14,12 +26,14 @@ std::weak_ptr<ESD>		THIS::createEnvironShadowDirectional()
 	return std::weak_ptr<ESD>();
 }
 /*
-   void				THIS::setTexture(std::shared_ptr<neb::fnd::itf::shared> texture)
-   {
-   }
-   */
-void				THIS::init(parent_t * const parent)
+std::weak_ptr<neb::fnd::environ::shadow::Directional>		THIS::createEnvironShadowDirectional()
 {
-	setParent(parent);
+	return neb::fnd::environ::util::Parent::createEnvironShadowDirectional();
 }
+std::weak_ptr<neb::fnd::environ::shadow::Point>		THIS::createEnvironShadowPoint()
+{
+	return neb::fnd::environ::util::Parent::createEnvironShadowPoint();
+}
+*/
+
 
